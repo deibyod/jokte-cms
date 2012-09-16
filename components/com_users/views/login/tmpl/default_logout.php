657e116b2b78ad8fe8b5e381d2beaedbfc6d0592
snaphuman <fhernandezn@gmail.com>
2012-09-16 14:42:18 -0500

    [logout] defino selectores al boton de desconexión permitiendo la transición css

--- components/com_users/views/login/tmpl/default_logout.php
+++ components/com_users/views/login/tmpl/default_logout.php
@@ -32,8 +32,8 @@ defined('_JEXEC') or die;
 	</div>
 	<?php endif ; ?>
 
-	<form action="<?php echo JRoute::_('index.php?option=com_users&task=user.logout'); ?>" method="post">
-		<div>
+	<form id="logout-form" action="<?php echo JRoute::_('index.php?option=com_users&task=user.logout'); ?>" method="post">
+		<div class="form-controls">
 			<button type="submit" class="button"><?php echo JText::_('JLOGOUT'); ?></button>
 			<input type="hidden" name="return" value="<?php echo base64_encode($this->params->get('logout_redirect_url', $this->form->getValue('return'))); ?>" />
 			<?php echo JHtml::_('form.token'); ?>
